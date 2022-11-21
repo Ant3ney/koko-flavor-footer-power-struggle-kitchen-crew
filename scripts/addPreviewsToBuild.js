e49f3c2824745e2c3d8e9be089ac7b972142e98f
Anthony Cavuoti <anthonycavuoti@gmail.com>
2022-11-21 11:34:49 -0800

    Remove discord test and improve preview description

--- scripts/addPreviewsToBuild.js
+++ scripts/addPreviewsToBuild.js
@@ -31,11 +31,10 @@ module.exports = function addBuildPreviewsToBuild() {
                     <meta property='og:title' content='ココFlavor Fodder.' />
                     <meta
                         property='og:description'
-                        content='John the manager gets transferred and all of s sudden there is a huge void in authority in ココ. The employees begin there quest to gain power in ココ.  Dave makes the first move for power but is quickly shot down by his peers, Cedric quietly makes moves along with Vicky. Mean while Keith and Xander are moving full steam ahead towards power. '
+                        content='John the manager gets transferred and all of a sudden there is a huge void in authority in ココ. The employees begin there quest to gain power in ココ.  Christian makes the first move for power but is quickly shot down by his peers, Kasey loudly makes power moves along with Vicky via nitpicking others company group chats. Meanwhile You move full steam ahead towards power. '
                     />
                     <meta property='og:image' content='https://serene-williams-bb8bee.netlify.app/images/KoKo/logosmall.jpg' />
                     <meta property='og:image' content='https://serene-williams-bb8bee.netlify.app/images/KoKo/logosmall.jpg' />
-                    <meta name="discord:image" content="https://cdn.sanity.io/images/2a4pwebi/production/6d3ac4dc8fe0f4f298888ee9097bdff467d7452c-370x282.png">
                 `;
 						const newContent = `${workableContent[0]}</title>${previews}${
 							workableContent[1] ? workableContent[1] : ''
